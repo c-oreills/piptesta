cea67a98d30db3a1f79e14bf4cb91cbc9675cc83
Christy O'Reilly <christy@oreills.co.uk>
2014-09-10 22:18:40 +0100

    Update deps

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     name='piptesta',
     description=desc,
     packages=['piptesta'],
+    install_requires=['piptestb==dev'],
+    dependency_links=[
+        'git+ssh://git@github.com/c-oreills/piptestb.git@9ff93995a260f3eafdecc57b3e3377a378692d36#egg=piptestb-dev'
+    ],
     long_description=desc,
 )
